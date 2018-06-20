8cfcac9e0707b4aa4f457ef8e32ecf01d05abf46
David Coy <me@david-coy.com>
2018-06-19 20:09:48 -0500

    Revert "Update server.js"
    This reverts commit 05980ec02f43569fce966b056a17e34438291f0f.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 
-// PRODUCTION ONLY
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // app middleware
@@ -13,7 +12,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-// PRODUCTION ONLY
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
