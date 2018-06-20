018b5fb3adc53095b12c2bb76f159b7f9459b530
David Coy <me@david-coy.com>
2018-06-19 19:46:20 -0500

    User and Product data seeded

---
+++ db/seeds/Product.js
@@ -0,0 +1,15 @@
+// MongoDB Query
+
+db.products.insertMany([
+  {_id:1,name: "Avocado",description:"A fruit",price: 2.29 },
+  {_id:2,name: "Apple",description:"A fruit",price: 1.35},
+  {_id:3,name: "Chicken Breast",description:"Delicious Chicken",price: 6.86},
+  {_id:4,name: "Angus Steak",description:"Angus Steak",price: 32.54},
+  {_id:5,name: "French Bread",description:"Delicious French Bread",price: 5.72},
+  {_id:6,name: "Whole Milk",description:"Milk",price: 3.89},
+  {_id:7,name: "Romaine Lettuce",description:"Just a head of lettuce",price: 2.73},
+  {_id:8,name: "Spinach",description:"Dark leafy greens",price: 2.68},
+  {_id:9,name: "Strawberries",description:"Ripe, Red and sweet",price: 4.59},
+  {_id:10,name: "Walnuts",description:"Crunchy",price: 7.64},
+  {_id:11,name: "Blue Cheese",description:"Creamy cheese",price: 8.36}
+]);
---
+++ db/seeds/User.js
@@ -0,0 +1,15 @@
+//MongoDB Query
+
+db.users.insertMany([
+  {_id:1,username: "adavidson",name:"Alison Davidson",email: "adavidson@example.com"},
+  {_id:2,username: "chartman",name:"Chance Hartmann",email: "chartman@example.com"},
+  {_id:3,username: "shanahan",name:"Shawn Shanahan",email: "shanahan@example.com"},
+  {_id:4,username: "eanderson",name:"Emmitt Anderson",email: "eanderson@example.com"},
+  {_id:5,username: "tklein",name:"Tony Klein",email: "tklein@example.com"},
+  {_id:6,username: "rstreich",name:"Reggie Streich",email: "rstreich@example.com"},
+  {_id:7,username: "shoeger",name:"Sigrid Hoeger",email: "shoeger@example.com"},
+  {_id:8,username: "rtromp",name:"Reid Tromp",email: "rtromp@example.com"},
+  {_id:9,username: "bhills",name:"Beau Hills",email: "bhills@example.com"},
+  {_id:10,username: "cziemann",name:"Colten Ziemann",email: "cziemann@example.com"},
+  {_id:11,username: "hlowe",name:"Hope Lowe",email: "hlowe@example.com"}
+]);
