6f8a463442f4a90826de0c385c212a9316ece14e
David Coy <me@david-coy.com>
2018-06-24 22:47:06 -0500

    Browser Routing implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,63 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
+import Signup from "./components/Signup/Signup";
+import LoginForm from "./components/LoginForm/LoginForm";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      username: null
+    };
+
+    this.getUser = this.getUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+  }
+  componentDidMount() {
+    this.getUser();
+  }
+  updateUser(userObject) {
+    this.setState(userObject);
+  }
+
+  getUser() {
+    axios.get("/user/").then(res => {
+      console.log(`GET User Response: ${res.data}`);
+      if (res.data.user) {
+        this.setState({
+          loggedIn: true,
+          username: res.data.user.username
+        });
+      } else {
+        this.setState({
+          loggedIn: false,
+          username: null
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        <h1 className="App-intro">
-          Here is where the front-end of our app will live
-        </h1>
+        <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+        {this.state.loggedIn && <p>Join the party, {this.state.username}!</p>}
+        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/login"
+          render={() => <LoginForm updateUser={this.updateUser} />}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={() => <Signup />}
+        />
       </div>
     );
   }
