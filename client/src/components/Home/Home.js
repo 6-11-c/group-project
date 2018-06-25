58dcd8d29c14267217638ce621bb85873ff6d342
David Coy <me@david-coy.com>
2018-06-24 23:04:13 -0500

    Show props for testing purposes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
+const Home = (props) => {
+  if (props.user) {
+    return (
+      <div className="Home">
+        <p>Current User: </p>
+        <h1>Hello!</h1>
+        <code>
+          {JSON.stringify(props.user)}
+        </code>
+      </div>
+    )
+  } else {
+    return (
+      <div className="Home">
+        <p>Current User:</p>
+        <code>
+          {JSON.stringify(props)}
+        </code>
+      </div>
+    )
+  }
+}
 
-
-
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export default Home;
