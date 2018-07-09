c749bbd623428839f5bc3feeb95d434e1391a923
Josh Butler <joshua.butler@my.scitexas.edu>
2018-07-09 19:32:08 -0400

    ImageSlider Component, Home component revamp

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+import ImageSlider from './ImageSlider';
 
 const Home = (props) => {
-  if (props.user) {
-    return (
-      <div className="Home">
-        <p>Current User: </p>
-        <h1>Hello!</h1>
-        <code>
-          {JSON.stringify(props.user)}
-        </code>
-      </div>
-    )
-  } else {
-    return (
-      <div className="Home">
-        <p>Current User:</p>
-        <code>
-          {JSON.stringify(props)}
-        </code>
-      </div>
-    )
-  }
-}
+  return (
+    <div>
+      <Jumbotron className="w-50 mx-auto m-5 bg-dark text-light">
+        <h1 className="display-5">Welcome to <em><strong>Epic Produce!</strong></em></h1>
+        <p className="lead">Here you will find a MASSIVE scale of produce.</p>
+        <hr className="my-2" />
+        <p>If you sign up with us, you will get a member discount on your first purchase and special weekly/monthly sales. Checkout some pictures of our epic produce below!</p>
+        <p className="lead">
+          <Button color="success" href="/signup">Sign Up!</Button>
+        </p>
+      </Jumbotron>
+
+      <ImageSlider />
+    </div>
+  );
+};
+
+
+// const Home = (props) => {
+//   if (props.user) {
+//     return (
+//       <div className="Home">
+//         <p>Current User: </p>
+//         <h1>Hello!</h1>
+//         <code>
+//           {JSON.stringify(props.user)}
+//         </code>
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div className="Home">
+//         <p>Current User:</p>
+//         <code>
+//           {JSON.stringify(props)}
+//         </code>
+//       </div>
+//     )
+//   }
+// }
 
 export default Home;
--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Button, Form, FormGroup, Input, FormText, Badge } from 'reactstrap';
+import { Button, Form, FormGroup, Input, FormText } from 'reactstrap';
 import './Signup.css';
 
 class Signup extends Component {
