314e70665bb7b506b181d7ebe3d8e750eed7dd14
David Coy <me@david-coy.com>
2018-07-25 18:05:33 -0500

    Fixing URL parsing

--- db/database.js
+++ db/database.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 
 const uri = `mongodb://${process.env.DBUSERNAME}:${process.env.DBPASS}@ds261440.mlab.com:61440/swd109j`
 
-mongoose.connect(uri).then(() => {
+mongoose.connect(uri, { useNewUrlParser: true }).then(() => {
   console.log('Connected to mLab');
 }, err => {
   console.log(`Error connecting to mLab: ${err}`)
