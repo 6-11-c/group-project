df76d95b30d152d0e05690aa94cd566e868b2321
David Coy <me@david-coy.com>
2018-07-25 23:25:02 -0500

    Serve React via Proxy

--- app.js
+++ app.js
@@ -55,8 +55,20 @@ app.use((err, req, res, next) => {
 // app.use(express.static(path.join(__dirname, "client/build")));
 
 // Production Only
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+// app.use(express.static('./client/build'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
 
 module.exports = app;
