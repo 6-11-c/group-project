2b5572e288b78501791e307e51586dabf17c93bd
David Coy <me@david-coy.com>
2018-07-26 09:31:54 -0500

    Signup endpoint updated

--- app.js
+++ app.js
@@ -61,12 +61,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Production Only
-// app.use(express.static(path.join(__dirname, "client/build")));
-
-// app.use(express.static('./client/build'));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
 module.exports = app;
