9cca1081c017021ba08a962d2ea1d5ecdfd66ea5
David Coy <me@david-coy.com>
2018-07-26 00:20:28 -0500

    Trying homepage serving again

--- app.js
+++ app.js
@@ -54,6 +54,16 @@ app.use((err, req, res, next) => {
 // Production Only
 // app.use(express.static(path.join(__dirname, "client/build")));
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  });
+}
+
 // app.use(express.static('./client/build'));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/client/build/index.html'));
