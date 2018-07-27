ede81c8faae40418c8d7e308b02d7c7ebfdd73b3
David Coy <me@david-coy.com>
2018-07-27 11:42:23 -0500

    Products rendering

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
-import { Container, Row, Col, Button, FormText } from 'reactstrap';
-import SearchBar from './SearchBar';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Container, Row, Col, Button, FormText } from "reactstrap";
+import SearchBar from "./SearchBar";
+import axios from "axios";
 
 class Product extends Component {
-
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       products: []
-    }
+    };
   }
 
   componentDidMount = () => {
-    axios.get('https://staging-agile-springs-53811.herokuapp.com/products').then((results, err) => {
-      const product = results.data;
-      console.log(product);
-      this.setState({ results });
-    }).catch(err => {
-      console.log(err);
-    });
-  }
+    axios
+      .get("https://staging-agile-springs-53811.herokuapp.com/products")
+      .then(results => {
+        const product = results.data;
+        // console.log('products', product);
+        this.setState({ products: product.products });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
-    console.log(this.state.products);
+    // console.log('products', this.state.products);
     return (
       <div>
         <SearchBar />
         <Container className="pt-5">
-          <h2 className="row justify-content-center text-light">Our Epic Produce!</h2>
-          {this.state.products.map(product =>
-            <Row>
-              <Col key={product._id} className="pt-2">
+          <h2 className="row justify-content-center text-light">
+            Our Epic Produce!
+          </h2>
+          {this.state.products.map((product, index) => (
+            <Row key={index}>
+              <Col className="pt-2">
                 <img className="img-fluid" src={product.productImage} alt="" />
                 <FormText color="light">{product.name}</FormText>
                 <FormText color="light">{product.price}</FormText>
                 <Button color="light">Add to Order</Button>
               </Col>
             </Row>
-          )}
+          ))}
         </Container>
       </div>
     );
