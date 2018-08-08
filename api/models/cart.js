3f500c575e63c486efc95cf5be0dcc30609c2b70
Josh Butler <joshua.butler@my.scitexas.edu>
2018-08-08 19:18:00 -0400

    update changes

--- api/models/cart.js
+++ api/models/cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const cartSchema = mongoose.Schema({
@@ -7,13 +6,3 @@ const cartSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("Cart", cartSchema);
-=======
-const mongoose = require("mongoose");
-
-const cartSchema = mongoose.Schema({
-  items: Array,
-  owner: String
-});
-
-module.exports = mongoose.model("Cart", cartSchema);
->>>>>>> f17907b253f4c2515d7742013541fd052cad68b2
